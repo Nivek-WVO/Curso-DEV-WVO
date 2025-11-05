f84e82eea9a7a1ede1c3a5e4a2066bc4eebcaf0d
Nivek-WVO <Kevinnivek2024@gmail.com>
2025-11-05 13:19:44 +0000

    adicionando textos e estilo para home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,53 @@
+import styles from "../CSS/estilo.module.css";
+
 function Home() {
-  return <h1>Vá trabalhar!</h1>;
+  return (
+    <div className={styles.caixa}>
+      <h1>Vá trabalhar!</h1>
+      <p>
+        1. Se todo mundo está fazendo de uma maneira, há uma boa chance de você
+        encontrar seu nicho indo exatamente na direção oposta. Sam Walton,
+        fundador do Walmart
+      </p>
+      <p>
+        2. O fracasso não existe. É apenas uma mudança de direção. Alejandro
+        Jodorowsky, cineasta
+      </p>
+      <p>
+        3. Não tenha medo de desistir do bom para ir para o grande. John D.
+        Rockefeller, magnata e filantropo
+      </p>
+      <p>
+        4. Muito trabalho duro está escondido atrás de coisas boas. Ralf Lauren,
+        estilista
+      </p>
+      <p>
+        5. Para cada dois minutos de glamour, há oito horas de trabalho duro.
+        Jessica Savitch, jornalista ex-âncora da NBC
+      </p>
+      <p>
+        6. Sorte? Eu não sei nada sobre sorte. Sorte para mim é outra coisa:
+        trabalho duro e perceber o que é oportunidade e o que não é. Lucille
+        Ball, comediante e atriz
+      </p>
+      <p>
+        7. Talento não é nada sem persistência. Dean Crawford, escritor
+        britânico
+      </p>
+      <p>
+        8. Quanto maior o obstáculo, mais glória em superá-lo. Molière, autor de
+        teatro, batizado Jean-Baptiste Poquelin
+      </p>
+      <p>
+        9. Transforme suas feridas em sabedoria. Oprah Winfrey, apresentadora,
+        jornalista e atriz
+      </p>
+      <p>
+        10. Se isso te assusta, pode ser uma boa coisa para tentar. Seth Godin,
+        escritor e orador
+      </p>
+    </div>
+  );
 }
 
 export default Home;
